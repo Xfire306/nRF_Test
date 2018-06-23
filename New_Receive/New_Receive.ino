929b43b3671908c669ba4924bab3b46e1a19f6b8
Alex Tam <alextam71@gmail.com>
2018-06-22 18:57:39 -0700

    Add files via upload

---
+++ New_Receive/New_Receive.ino
@@ -0,0 +1,78 @@
+
+#include <SPI.h>
+#include "RF24.h"
+
+/*
+Akhil and Alex:
+This code continuously waits to receive a signal. When one is recieved information 
+is printed to the console. Then it waits to receive a signal again. Begin running this code
+before running the transmitter code.
+
+Here is how events should occur:
+1) Signal is transmitted
+2) Transmitter is paused
+3) Signal is received and printed to console
+4) Transmitter is unpaused
+5) Return to step 1
+*/
+
+RF24 radio(9,10);
+byte addresses[][6] = {"1Node","2Node"};
+double begin_time;
+int count = 0;
+double elapsed_sum = 0;
+int average_len = 1000;
+double elapsed = 0;
+//Time variables
+double now = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("*** Please wait until transmitter begins"));
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1,addresses[0]);
+  radio.startListening();
+  
+}
+
+void loop() {
+    double sent_time;
+    begin_time = micros();
+    if(radio.available()){
+        while (radio.available()) {                                   // While there is data ready
+          radio.read( &sent_time, sizeof(double) );             // Get the payload
+        }                                           
+      now = micros();
+      count++;
+      elapsed =  (now - begin_time);
+      elapsed_sum += elapsed;
+      Serial.print("Count: ");
+      Serial.println(count);
+      Serial.print("Sent time: ");        
+      Serial.print(begin_time);     
+      Serial.print(", received time: ");
+      Serial.print(now);
+      Serial.print(", Elapsed: ");
+      Serial.println(elapsed);
+      
+      
+
+//      delay(10);
+      }
+    
+    if(count == average_len){
+      double average = elapsed_sum / ( (double) average_len);
+      Serial.print("Average of multiple times ");
+      Serial.println(average);
+      delay(5000);
+      average = 0;
+      elapsed_sum = 0;
+      now = 0;
+      count = 0;
+    }
+ }
+
+
+
---
+++ New_Transmit/New_Transmit.ino
@@ -0,0 +1,70 @@
+#include <SPI.h>
+#include "RF24.h"
+
+
+/*
+Akhil and Alex:
+This code is meant to send a packet with the time current time every 1 second. 
+It is important that it takes less than 1 second to receive or 
+two packets will be sent and only one received. If the code doesn't run
+hopefully you can debug any errors...
+Start running the receiver code. Then run this code.
+*/
+
+
+RF24 radio(7,8);
+byte addresses[][6] = {"1Node","2Node"};
+int role = 0;
+
+double start_time = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
+  radio.stopListening();
+}
+
+void loop() {
+   if(role == 1)
+   {
+    start_time = micros();                            
+     if (!radio.write( &start_time, sizeof(double) )){
+       Serial.println(F("stanby"));
+//       delay(5000);
+     }
+     else{
+      Serial.print("Sending at time: ");
+      Serial.println(start_time);
+//      delay(100);
+     }
+//    delay(500); //ASSUMES SIGNAL IS RECEIVED IN UNDER ONE SECOND
+   }
+
+    //Serial Monitor Enable
+if ( Serial.available() )
+  {
+    char c = toupper(Serial.read());
+    if ( c == 'T' && role == 0 ){      
+      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
+      role = 1;                  // Become the primary transmitter (ping out)
+    
+   }else
+    if ( c == 'R' && role == 1 ){
+      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
+       role = 0;                // Become the primary receiver (pong back)
+       radio.startListening();
+       
+    }
+  }
+
+   
+}
+
+
+
+
